b9933088c9dd093fcd38457339547d41e7a84cf0
neil.yu <neil.yu@dynasafe.com.tw>
2022-05-17 10:01:15 +0800

    20220517 1001 scdf java01 test

---
+++ testjob01/src/main/java/com/milkliver/springcloud/test/testjob01/jobs/Job01.java
@@ -0,0 +1,21 @@
+package com.milkliver.springcloud.test.testjob01.jobs;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Job01 {
+
+	private static final Logger log = LoggerFactory.getLogger(Job01.class);
+
+	@PostConstruct
+	public void run() {
+
+		log.info("this is java job01");
+
+	}
+
+}
