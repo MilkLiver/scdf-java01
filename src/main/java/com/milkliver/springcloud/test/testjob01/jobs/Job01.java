81814fa7efaf4853b4d72d79bd359ae4f8428868
neil.yu <neil.yu@dynasafe.com.tw>
2022-06-07 09:46:22 +0800

    20220607-0946 test

--- src/main/java/com/milkliver/springcloud/test/testjob01/jobs/Job01.java
+++ src/main/java/com/milkliver/springcloud/test/testjob01/jobs/Job01.java
@@ -10,21 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class Job01 {
 
-	@Value("${spring.cloud.task.executionid:#{null}}")
-	Integer taskid;
-
 	private static final Logger log = LoggerFactory.getLogger(Job01.class);
 
 	@PostConstruct
 	public void run() {
 
-		if (taskid != null) {
-			log.info("taskid: " + String.valueOf(taskid));
-		} else {
-			log.info("taskid: null");
-		}
-
-		log.info("2022,06,06 18:39");
+		log.info("2022,06,07 09:45");
 		log.info("this is java job01");
 
 	}
